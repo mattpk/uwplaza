41ae8a23661b67964d68f1ffea35cc6198f9e38b
Erik Trinh <trinh.erik@gmail.com>
2016-03-05 04:17:29 -0500

    anotha one

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	var itemList = getList();
 	console.log(itemList);
 	var list = document.getElementById("submit-drop");
-	Arrays.prototype.forEach.call(itemList, function(item) {
+	Array.prototype.forEach.call(itemList, function(item) {
 		var itemEl = document.createElement("li");
 		itemEl.innerHTML = item;
 		list.appendChild(itemEl);
