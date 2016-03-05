c6f9efe782e560562888d6c15db7d9a227a8c59a
mattpk <mattpkqwe@gmail.com>
2016-03-05 04:50:33 -0500

    fuck

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 	//$.getScript("requests.js", function*() {
 	//	console.log("main loaded, require requests");
 	//});
+	console.log("document ready");
 
 
 	var getList = function () {
 		$.post("ajaxReq.php", {req: "restaurants"}).done(function(data) {
+
 			var itemList = JSON.parse(data);
 			console.log(itemList);
 			var list = document.getElementById("submit-drop");
