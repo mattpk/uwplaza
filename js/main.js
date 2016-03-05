0dabbe7141412d837149fca4788841c913ca3c30
Erik Trinh <trinh.erik@gmail.com>
2016-03-05 04:03:52 -0500

    fix attempt

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log("main loaded, require requests");
 
 $(document).ready(function() {
 	//populates list item
-	var itemList = req.getList();
+	var itemList = getList();
 	var list = document.getElementById("submit-drop");
 	itemList.forEach(function(item) {
 		var itemEl = document.createElement("li");
