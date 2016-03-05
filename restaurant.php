4c4054d9e8f3e0ca917b5587b7a334349a372123
Benson Guo <benson1guo@gmail.com>
2016-03-05 11:16:26 -0500

    jumbo

---
+++ restaurant.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Restaurants of UWPlaza</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="js/requests.js"></script>
+    <script src="js/main.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+
+
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+    <div class="col-sm-2">
+    <a href="index.php"><img src="EatPlazaLogo2.png" class="img-rounded" alt="Logo" width="263" height="163"></a>
+    </div>
+    <div class="col-sm-10">
+    <div class="bottom_aligner">
+	<h1>The Panino Challenge</h1>
+    </div>
+    </div>
+    </div>
+	<table class="table table-bordered">
+    
+    
+  </table>
+</div>
+
+
+
+</body>
+
+</html>
