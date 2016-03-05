b267ac38dbd869a3a9a50d22762c8ff95af8a81b
Erik Trinh <trinh.erik@gmail.com>
2016-03-05 04:11:21 -0500

    anotha one

--- js/requests.js
+++ js/requests.js
@@ -6,6 +6,7 @@ var reqRestaurant = function(name) {
 
 var getList = function () {
 	$.post("ajaxReq.php", {req: "restaurants"}, function(data) {
+		console.log(data);
 		return JSON.parse(data);
 	});
 }
