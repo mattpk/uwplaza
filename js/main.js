eaee48c063bae67367e94cf7fa209867fb6eac86
Erik Trinh <trinh.erik@gmail.com>
2016-03-05 04:45:03 -0500

    anotha one

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ $(document).ready(function() {
 	var getList = function () {
 		$.post("ajaxReq.php", {req: "restaurants"}).done(function(data) {
 			var itemList = JSON.parse(data);
+			console.log("response succeed");
 			console.log(itemList);
 			var list = document.getElementById("submit-drop");
-			Array.prototype.forEach.call(itemList, function(item) {
+			itemList.forEach(function(item) {
+				console.log(item);
 				var itemEl = document.createElement("li");
 				itemEl.innerHTML = item;
 				list.appendChild(itemEl);
