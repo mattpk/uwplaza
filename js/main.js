ae0574c2638e5f3ef7e4ccbdbffcccda089b7db0
Erik Trinh <trinh.erik@gmail.com>
2016-03-05 03:59:10 -0500

    trying new require module system

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-var req = require("./requests");
+require(['js/requests'], function(req) {
+	console.log("main loaded, require requests");
 
-$(document).ready(function() {
-	//populates list item
-	var itemList = req.getList();
-	var list = document.getElementById("submit-drop");
-	itemList.forEach(function(item) {
-		var itemEl = document.createElement("li");
-		itemEl.innerHTML = item;
-		list.appendChild(itemEl);
+	$(document).ready(function() {
+		//populates list item
+		var itemList = req.getList();
+		var list = document.getElementById("submit-drop");
+		itemList.forEach(function(item) {
+			var itemEl = document.createElement("li");
+			itemEl.innerHTML = item;
+			list.appendChild(itemEl);
+		});
+
 	});
-	
 });
